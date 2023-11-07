e13bb37eae624ef1b6f5442b69a5562632a3689b
SSQ <110625524+ssq0-0@users.noreply.github.com>
2023-11-08 00:22:58 +0300

    Update user_panel.py

--- user/user_panel.py
+++ user/user_panel.py
@@ -1,9 +1,13 @@
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.filters.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 from aiogram import Router, types, F
 from aiogram.types import Message
 from aiogram.filters import Command
-from buttons.buttons import buttons_panel, send_catalog_keyboard, account_info, wallet_info
-# from registrations.registration import
+from buttons.buttons import buttons_panel, send_category_keyboard, account_info, wallet_info, _send_cups, valid_articul
+from registrations.registration import start
+
+class AwaitArct(StatesGroup):
+    articul = State()
 
 router = Router()
 
@@ -15,12 +19,12 @@ async def react_to_menu(message: Message):
 
 @router.message(F.text == 'Каталог')
 async def send_catalog(message: Message):
-    await send_catalog_keyboard(message)
+    await send_category_keyboard(message)
 
 
 @router.callback_query(F.data == 'Cups')
 async def cup_catalog(callback: types.CallbackQuery):
-    await callback.message.answer('Вы выбрали кружки')
+    await _send_cups()
 
 
 @router.callback_query(F.data == 'Sweatshirt')
@@ -39,8 +43,8 @@ async def print_account_info(message: Message):
 
 
 @router.callback_query(F.data == 'Change')
-async def change_user_info(callback: types.CallbackQuery):
-    await callback.message.answer('Пагади')
+async def change_user_info(callback: types.CallbackQuery, state: FSMContext):
+    await start(callback.message, state)
 
 
 @router.message(F.text == 'Кошелек')
@@ -51,3 +55,17 @@ async def print_wallet_info(message: Message):
 @router.callback_query(F.data == 'Transfer')
 async def transfer_from_to(callback: types.CallbackQuery):
     await callback.message.answer('Трансфера потом')
+
+
+@router.message(F.text == 'Удалить товар')
+async def pre_delete_product_from_catalog(message: Message, state: FSMContext):
+    await message.answer('Напишите артикул удаляемого товара')
+    await state.set_state(AwaitArct.articul)
+
+
+@router.message(AwaitArct.articul)
+async def _delete_product_from_catalog(message: Message, state: FSMContext):
+    articul = message
+    await state.clear()
+    await valid_articul(articul)
+
