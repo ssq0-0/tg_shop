c1573e0f5b20edf181afa3cccf84da1c24705051
SSQ <110625524+ssq0-0@users.noreply.github.com>
2023-11-07 20:58:03 +0300

    Update buttons.py

--- buttons/buttons.py
+++ buttons/buttons.py
@@ -1,8 +1,12 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram import types
 from aiogram.types import Message
+from config.base import get_account_info
+
 
 """Раздел обработки кнопок, связанных с каталогом товаров"""
+
+
 async def buttons_panel(message: Message):
     kb = [
         [
@@ -39,19 +43,16 @@ async def send_catalog_keyboard(message: Message):
 
 
 async def account_info(message: Message):
-    account = {
-        'Имя': 'Тут',
-        'Возраст': 'пока что',
-        'Номер телефона': 'ничего',
-        'Адрес': 'нет',
-    }
+    account_info = get_account_info(message.from_user.id)
+    name, age, number, address = account_info[1], account_info[2], account_info[3], account_info[4]
+
     builder = InlineKeyboardBuilder()
     builder.add(types.InlineKeyboardButton(
             text='Изменить', callback_data='Change')
     )
-    account_text = "\n".join([f"{key}: {value}" for key, value in account.items()])
+    text = f"ID: {message.from_user.id}\nИмя: {name}\nВозраст: {age}\nНомер телефона: {number}\nАдрес: {address}"
 
-    await message.answer(account_text, reply_markup=builder.as_markup())
+    await message.answer(text, reply_markup=builder.as_markup())
 
 
 """раздел обработки кнопки кошелька"""
@@ -63,6 +64,6 @@ async def wallet_info(message: Message):
             text='Перевести другому пользователю', callback_data='Transfer'
         ))
 
-    await message.answer('пока ничего нет', reply_markup=builder.as_markup())
+    await message.answer('Ты пока бомж', reply_markup=builder.as_markup())
 
 
