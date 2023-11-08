e6e0a4ee81018bb4b2317558d35720cbe0960845
SSQ <110625524+ssq0-0@users.noreply.github.com>
2023-11-08 21:40:57 +0300

    Update user_panel.py

--- user/user_panel.py
+++ user/user_panel.py
@@ -5,9 +5,19 @@ from aiogram.types import Message
 from aiogram.filters import Command
 from buttons.buttons import buttons_panel, send_category_keyboard, account_info, wallet_info, _send_cups, valid_articul
 from registrations.registration import start
+from config.base import catalog
+
+articul_product, name_product, price_product, count_product, category_product = "", "", 0, 0, ""
+
 
 class AwaitArct(StatesGroup):
     articul = State()
+    name_product = State()
+    price_product = State()
+    count_product = State()
+    ctaegory_product = State()
+    delete = State()
+
 
 router = Router()
 
@@ -24,7 +34,7 @@ async def send_catalog(message: Message):
 
 @router.callback_query(F.data == 'Cups')
 async def cup_catalog(callback: types.CallbackQuery):
-    await _send_cups()
+    await _send_cups(category='Cups')
 
 
 @router.callback_query(F.data == 'Sweatshirt')
@@ -57,15 +67,62 @@ async def transfer_from_to(callback: types.CallbackQuery):
     await callback.message.answer('Трансфера потом')
 
 
-@router.message(F.text == 'Удалить товар')
-async def pre_delete_product_from_catalog(message: Message, state: FSMContext):
-    await message.answer('Напишите артикул удаляемого товара')
+@router.message(F.text == 'Добавить товар')
+async def append_product(message: Message, state: FSMContext):
+    await message.answer('Введите артикул товара')
     await state.set_state(AwaitArct.articul)
 
 
 @router.message(AwaitArct.articul)
-async def _delete_product_from_catalog(message: Message, state: FSMContext):
-    articul = message
+async def _input_arcticul(message: Message, state: FSMContext):
+    global articul_product
+    articul_product = message.text
+    await message.answer('Введите название товара')
+    await state.set_state(AwaitArct.name_product)
+
+
+@router.message(AwaitArct.name_product)
+async def _input_name_product(message: Message, state: FSMContext):
+    global name_product
+    name_product = message.text
+    await message.answer('Введите цену товара')
+    await state.set_state(AwaitArct.price_product)
+
+
+@router.message(AwaitArct.price_product)
+async def _input_price_product(message: Message, state: FSMContext):
+    global price_product
+    price_product = message.text
+    await message.answer('Введите количество товара')
+    await state.set_state(AwaitArct.count_product)
+
+
+@router.message(AwaitArct.count_product)
+async def _input_count_price(message: Message, state: FSMContext):
+    global count_product
+    count_product = message.text
+    await message.answer('Введите категорию товара')
+    await state.set_state(AwaitArct.ctaegory_product)
+
+
+@router.message(AwaitArct.ctaegory_product)
+async def _input_category_product(message: Message, state: FSMContext):
+    global category_product
+    category_product = message.text
+    catalog(articul_product, category_product, price_product, count_product, name_product)
+    await message.answer('Ожидаем добавления...')
     await state.clear()
-    await valid_articul(articul)
+
+
+@router.message(F.text == 'Удалить товар')
+async def react_to_delete(message: Message, state: FSMContext):
+    await message.answer('Напишите артикул удаляемого товара')
+    await state.set_state(AwaitArct.delete)
+
+
+@router.message(AwaitArct.delete)
+async def _delete_product_from_catalog(message: Message, state: FSMContext):
+    articul_product = message
+    await state.clear()
+    await valid_articul(articul_product)
 
