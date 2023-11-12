8324752b8e071818d86914b3260498ee9a8aee8d
SSQ <110625524+ssq0-0@users.noreply.github.com>
2023-11-12 13:03:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 import asyncio
-import logging
-import sys
-from aiogram import Bot, Dispatcher, types
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
-from aiogram.utils.markdown import hbold
-from aiogram import F
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-
-import buttons.buttons
+from aiogram import Bot, Dispatcher 
+from config.token import token
 import registrations.registration
 import user.user_panel
-from registrations.registration import *
-
-
 
 
 async def main():
-    bot = Bot(token='6946925296:AAHqpeenSDI6WsKFTM9YxzxfCHm_z9idAV4')
+    bot = Bot(token=token)
     dp = Dispatcher()
     dp.include_routers(registrations.registration.router, user.user_panel.router)
     await bot.delete_webhook(drop_pending_updates=True)
