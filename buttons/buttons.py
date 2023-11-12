5b198427d0a058e7634defd663da94b127268f48
SSQ <110625524+ssq0-0@users.noreply.github.com>
2023-11-12 13:03:17 +0300

    Update buttons.py

--- buttons/buttons.py
+++ buttons/buttons.py
@@ -14,7 +14,8 @@ async def buttons_panel(message: Message):
             [
                 types.KeyboardButton(text='Каталог'),
                 types.KeyboardButton(text='Аккаунт'),
-                types.KeyboardButton(text='Кошелек')
+                types.KeyboardButton(text='Кошелек'),
+                types.KeyboardButton(text='Купить товар')
             ],
         ]
         keyboards = types.ReplyKeyboardMarkup(
@@ -48,13 +49,13 @@ async def send_category_keyboard(message: Message):
     builder = InlineKeyboardBuilder()
     builder.add(
         types.InlineKeyboardButton(
-            text="Кружки", callback_data="Cups"
+            text="Кружки", callback_data="кружки"
         ),
         types.InlineKeyboardButton(
-            text="Свитшоты", callback_data="Sweatshirt"
+            text="Свитшоты", callback_data="свитшоты"
         ),
         types.InlineKeyboardButton(
-            text="Прочее", callback_data="Others"
+            text="Прочее", callback_data="прочее"
         )
     )
     await message.answer('Выберите раздел.', reply_markup=builder.as_markup())
@@ -65,10 +66,27 @@ async def _send_cups(callback_query, category):
     chat_id = callback_query.message.chat.id
 
     for cup in cups:
-        articul, name, price, count, url = cup[1], cup[2],cup[3], cup[4], cup[5]
+        articul, name, price, count, url = cup[2], cup[1], cup[3], cup[4], cup[5]
         response_message = f"Артикул: {articul}\nНазвание: {name}\nЦена: {price}\nКоличество: {count}\n Фото: {url}"
         await callback_query.bot.send_message(chat_id, response_message)
 
+async def _send_sweatshirt(callback_query, category):
+    sweatshirt = send_product_from_catalog(category)
+    chat_id = callback_query.message.chat.id
+
+    for sweatshirts in sweatshirt:
+        articul, name, price, count, url = sweatshirts[2], sweatshirts[1], sweatshirts[3], sweatshirts[4], sweatshirts[5]
+        response_message = f"Артикул: {articul}\nНазвание: {name}\nЦена: {price}\nКоличество: {count}\n Фото: {url}"
+        await callback_query.bot.send_message(chat_id, response_message)
+
+async def _send_others(callback_query, category):
+    others = send_product_from_catalog(category)
+    chat_id = callback_query.message.chat.id
+
+    for other in others:
+        articul, name, price, count, url = other[2], other[1], other[3], other[4], other[5]
+        response_message = f"Артикул: {articul}\nНазвание: {name}\nЦена: {price}\nКоличество: {count}\n Фото: {url}"
+        await callback_query.bot.send_message(chat_id, response_message)
 async def valid_articul(message):
     articul = message.text
     print(articul)
@@ -85,7 +103,7 @@ async def valid_articul(message):
 
 async def account_info(message: Message):
     account_info = get_account_info(message.from_user.id)
-    name, age, number, address = account_info[1], account_info[2], account_info[3], account_info[4]
+    name, age, number, address = account_info[1], account_info[3], account_info[4], account_info[5]
 
     builder = InlineKeyboardBuilder()
     builder.add(types.InlineKeyboardButton(
@@ -100,12 +118,15 @@ async def account_info(message: Message):
 
 
 async def wallet_info(message: Message):
+    account_info = get_account_info(message.from_user.id)
+    ballance = account_info[2]
+
     builder = InlineKeyboardBuilder()
     builder.add(
         types.InlineKeyboardButton(
             text='Перевести другому пользователю', callback_data='Transfer'
         ))
-
-    await message.answer('Ты пока бомж', reply_markup=builder.as_markup())
+    text = f"Ваш баланс: {ballance}"
+    await message.answer(text, reply_markup=builder.as_markup())
 
 
